14a35f4360bf3765a869799817c5cbb986d8cd08
fwmakc <fwmakc@mail.ru>
2021-06-02 13:44:07 +0300

    1.0

--- class/Form.php
+++ class/Form.php
@@ -9,18 +9,42 @@ use is\Helpers\Strings;
 use is\Masters\Modules\Master;
 use is\Masters\View;
 
+use is\Components\Config;
+use is\Components\State;
 use is\Components\Dom;
+use is\Components\Uri;
+
+// все операции, связанные с обработкой данных теперь находятся в апи
+// если раньше форма фактически управляла этими операциями,
+// то теперь это просто виджет формы
+// однако здесь все еще не хватает передачи данных:
+// - ключ с именем пользователя и паролем апи
+// - спам-защита в пустой скрытой ячейке
+// - отлов и вывод ошибок и возврат значей в ячейки
+// - информация о форме, с которой был запущен процесс
+// последнее нужно для очистки и валидации входных данных
+// хотя, возможно, это как раз не нужно,
+// т.к. управляться должно опять же не формой, а апи
+// соответственно, именно в настройках процессов апи мы задаем
+// очистку, валидацию и прочее
 
 class Form extends Master {
+	
+	public $returns;
 	
 	public function launch() {
 		
 		// если нет ключа, пробуем взять ключ из СЕО
 		
+		$uri = Uri::getInstance();
+		$this -> returns = $uri -> getData();
+		unset($uri);
+		
 		$sets = &$this -> settings;
 		
 		$this -> buildFields();
-		$this -> printFields();
+		$this -> buildForm();
+		//$this -> printFields();
 		
 	}
 	
@@ -31,7 +55,7 @@ class Form extends Master {
 		
 		$sets = &$this -> settings;
 		
-		$tags = ['select', 'textarea'];
+		$tags = ['select', 'textarea', 'list'];
 		
 		if (!System::typeIterable($sets['data'])) {
 			return;
@@ -43,6 +67,12 @@ class Form extends Master {
 			unset($attr['data'], $attr['options']);
 			
 			$tag = Objects::match($tags, $item['type']) ? $item['type'] : (System::typeIterable($item['data']) ? 'div' : 'input');
+			$rval = $this -> returns[$item['name']];
+			$returns = [
+				'value' => !System::typeOf($rval, 'iterable') ? $rval : null,
+				'array' => System::typeOf($rval, 'iterable') ? $rval : []
+			];
+			unset($rval);
 			
 			if (
 				$attr['name'] &&
@@ -51,7 +81,8 @@ class Form extends Master {
 				$attr['name'] .= '[]';
 			}
 			
-			$this -> elements[ $item['name'] ] = new Dom($tag);
+			$this -> ecreate($item['name'], $tag);
+			//$this -> elements[ $item['name'] ] = new Dom($tag);
 			
 			if (System::typeIterable($item['data'])) {
 				
@@ -64,8 +95,19 @@ class Form extends Master {
 						$it -> addCustom('value', $k);
 						$it -> addContent($i);
 						if (
-							($item['type'] === 'checkbox' || $item['type'] === 'radio') &&
-							$k === $item['options']['default']
+							(
+								$item['type'] === 'checkbox' ||
+								$item['type'] === 'radio'
+							) &&
+							(
+								(
+									$k === $item['options']['default'] &&
+									!$returns['value'] &&
+									!$returns['array']
+								) ||
+								$k === $returns['value'] ||
+								Objects::match($returns['array'], $k)
+							)
 						) {
 							$it -> addAttr('checked');
 						}
@@ -94,6 +136,45 @@ class Form extends Master {
 					}
 					unset($k, $i);
 					
+				} elseif ($tag === 'list') {
+					
+					$this -> eget($item['name']) -> setTag('input');
+					$this -> eget($item['name']) -> setCustom('list', $this -> get('instance') . '-' . $item['name'] . '-datalist');
+					
+					$print = null;
+					
+					foreach ($item['data'] as $k => $i) {
+						$it = new Dom('option');
+						if (System::type($k, 'numeric')) {
+							$it -> addCustom('value', $i);
+						} else {
+							$it -> addCustom('value', $k);
+							$it -> addContent($i);
+						}
+						if (System::set($item['options']['default'])) {
+							$this -> eget($item['name']) -> addCustom('placeholder', $item['options']['default']);
+						}
+						if (System::set($returns['value'])) {
+							$this -> eget($item['name']) -> addCustom('value', $returns['value']);
+						}
+						$print .= $it -> get();
+					}
+					unset($k, $i, $it);
+					
+					if (System::typeIterable($attr)) {
+						foreach ($attr as $kk => $ii) {
+							if ($ii === true) {
+								$this -> eget($item['name']) -> addAttr($kk);
+							} else {
+								$this -> eget($item['name']) -> addCustom($kk, $ii);
+							}
+						}
+						unset($kk, $ii);
+					}
+					
+					$content = $item['options']['before'] . '<datalist id="' . $this -> get('instance') . '-' . $item['name'] . '-datalist">' . $print . '</datalist>' . $item['options']['after'];
+					unset($print);
+					
 				} else {
 					
 					$print = null;
@@ -102,7 +183,15 @@ class Form extends Master {
 						$it = new Dom('option');
 						$it -> addCustom('value', $k);
 						$it -> addContent($i);
-						if ($k === $item['options']['default']) {
+						if (
+							(
+								$k === $item['options']['default'] &&
+								!$returns['value'] &&
+								!$returns['array']
+							) ||
+							$k === $returns['value'] ||
+							Objects::match($returns['array'], $k)
+						) {
 							$it -> addAttr('selected');
 						}
 						$print .= $it -> get();
@@ -125,12 +214,6 @@ class Form extends Master {
 					
 				}
 				
-				//echo htmlentities($content);
-				//echo '<br><br>';
-				//echo '+++';
-				
-				//$this -> elements[ $item['name'] ] = new Dom( Objects::match($tags, $item['type']) ? $item['type'] : 'input' );
-				
 			} else {
 				
 				$print = null;
@@ -146,6 +229,13 @@ class Form extends Master {
 					unset($kk, $ii);
 				}
 				
+				if (System::set($item['options']['default'])) {
+					$this -> eget($item['name']) -> addCustom('placeholder', $item['options']['default']);
+				}
+				if (System::set($returns['value'])) {
+					$this -> eget($item['name']) -> addCustom('value', $returns['value']);
+				}
+				
 				$content = $item['options']['before'] . $print . $item['options']['after'];
 				unset($print);
 				
@@ -155,9 +245,6 @@ class Form extends Master {
 			
 		}
 		unset($item);
-		
-		//echo htmlentities(print_r($this -> elements, 1)) . '<br>';
-		//echo htmlentities(print_r($this -> elements, 1)) . '<br>';
 		
 	}
 	
@@ -171,6 +258,41 @@ class Form extends Master {
 			$item -> print();
 		}
 		unset($item);
+		
+	}
+	
+	public function buildForm() {
+		
+		$this -> ecreate('form', 'form');
+		
+		$sets = &$this -> settings;
+		
+		if (!$sets['api']) {
+			return;
+		}
+		
+		$config = Config::getInstance();
+		$action = '/' . $config -> get('api:name') . '/' . Strings::replace($sets['api'], ':', '/') . '/';
+		
+		Objects::each($sets['form'], function($i, $k){
+			$this -> eget('form') -> addCustom($k, $i);
+		});
+		
+		$this -> eget('form') -> addCustom('method', $sets['method'] === 'post' || $sets['method'] === 'files' ? 'post' : 'get');
+		
+		if ($sets['method'] === 'files') {
+			$this -> eget('form') -> addCustom('enctype', 'multipart/form-data');
+		}
+		
+		$this -> eget('form') -> addCustom('action', $action);
+		
+	}
+	
+	public function printForm() {
+		
+		$this -> eget('form') -> open(true);
+		$this -> printFields();
+		$this -> eget('form') -> close(true);
 		
 	}
 	
--- templates/default.php
+++ templates/default.php
@@ -6,62 +6,9 @@ use is\Helpers\System;
 use is\Helpers\Objects;
 use is\Helpers\Strings;
 
-use is\Components\Uri;
-
-$uri = Uri::getInstance();
-
 $instance = $object -> get('instance');
 $sets = &$object -> settings;
 
-echo print_r($uri, 1);
-
-//echo print_r($object, 1);
-
-//$object -> eget('container') -> addClass('new');
-//$object -> eget('container') -> open(true);
-//$object -> eget('container') -> close(true);
-//$object -> eget('container') -> print();
-
-
+$object -> printForm();
 
 ?>
-
-<form method="post" action="">
-<select name="asfd[]" multiple>
-<?php
-foreach ($d as $key => $item) {
-?>
-<option<?= System::set($key) ? ' value="' . $key . '"' : null; ?>><?= $item; ?></option>
-<?php
-}
-unset($key, $item);
-?>
-</select>
-
-<input type="submit">
-</form>
-
-<div class="<?= $instance; ?>">
-	
-	<p><?= $sets['key']; ?></p>
-	
-	<?php
-		if (System::typeIterable($sets['array'])) {
-	?>
-	<ul>
-	<?php
-		foreach ($sets['array'] as $item) {
-	?>
-		<li><?= $item; ?></li>
-	<?php
-		}
-		unset($item);
-	?>
-	</ul>
-	<?php
-		}
-	?>
-	
-	<?php $object -> blocks('block'); ?>
-	
-</div>
